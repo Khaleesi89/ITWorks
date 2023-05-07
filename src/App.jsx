e8606298b3e5562e338ac6c73725cf8c6f1017c7
rosana <rosanasalva@yahoo.com.ar>
2023-05-07 19:18:48 -0300

    componente home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import { Navbar } from './components/Navbar/Navbar';
 import { Promotions } from './components/Promotions/Promotions';
-
+import {Home} from './components/Home/Home';
 function App() {
   
     return(
         <div>
             <Promotions/>
             <Navbar />
+            <Home/>
+            
+        
+            
+            
         </div>
     )
 
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+
+const enlace = 'Nuestros Servicios' ;
+
+
+
+ export const Home = () => {
+    return (
+        
+        <div className="home-container ">
+            <h2 className="slogan">La transformación digital </h2>
+            <h2 className="slogan" >que tu negocio necesita,</h2>
+            <h2 className="slogan">en un solo lugar.</h2>
+            <button className="btn">{enlace}</button>
+        </div>
+        
+    )
+ }
