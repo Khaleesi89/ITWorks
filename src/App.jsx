a08748def415265db2f9d1a14a661d0251dc8d19
RocioFarias09 <fariasrocio10@gmail.com>
2023-05-07 17:33:36 -0300

    Avance de seccion ofertas y servicios

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,28 @@ import './App.css'
 import { Navbar } from './components/Navbar/Navbar';
 import { Promotions } from './components/Promotions/Promotions';
 
+
+
+ 
+ 
+
+
+
+
 function App() {
   
     return(
         <div>
             <Promotions/>
             <Navbar />
+            
+            
+
         </div>
     )
 
 }
 
-export default App
+export default App;
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import Card from './components/Servicios/Cards'
+import Ofertas from './components/Servicios/Ofertas'
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Card />
+    <Ofertas />
+    
   </React.StrictMode>
   
 )
