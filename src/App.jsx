c5bf4ceb6aba6257a303523b65e099ef9d3febce
Marcia Klimisch <93682145+Khaleesi89@users.noreply.github.com>
2023-05-08 17:38:18 -0300

    modificando

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,18 @@
 import './App.css'
 import { Navbar } from './components/Navbar/Navbar';
 import { Promotions } from './components/Promotions/Promotions';
-<<<<<<< HEAD
 import {Home} from './components/Home/Home';
-=======
-
-
-
- 
- 
-
-
-
-
->>>>>>> 5b1613fffc8024343a6926e74225f7d3a200a390
 function App() {
   
     return(
         <div>
             <Promotions/>
             <Navbar />
-<<<<<<< HEAD
             <Home/>
             
         
             
             
-=======
-            
-            
-
->>>>>>> 5b1613fffc8024343a6926e74225f7d3a200a390
         </div>
     )
 
