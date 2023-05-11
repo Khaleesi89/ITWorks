0d646cf59fe2b547e612e957eb45e552352b733e
rogonra <95001533+rogonra@users.noreply.github.com>
2023-05-11 19:36:29 -0300

    cambios del home

---
+++ Home/Home.jsx
@@ -0,0 +1,24 @@
+
+import  Button  from "./Button"
+import Slogan from "./Slogan"
+
+
+
+export const Home = () => {
+    return (
+        <div className="home-container" >
+             <Slogan></Slogan>
+             <Button></Button>
+        </div>
+    )
+           
+              
+
+            
+
+        
+            
+            
+    
+
+}
