70ca49ce5e725d264aa9000e9a1fae520feb31a4
maxijv97 <maximilianomansilla1997@gmail.com>
2023-05-19 20:02:14 -0300

    Actualize card y corregi css

--- src/components/AboutUs/CardsUS.jsx
+++ src/components/AboutUs/CardsUS.jsx
@@ -1,55 +1,55 @@
 import React from 'react'
 import CardUS from './CardUS'
-import imagen1 from "./assets/imagen1.png";
-import imagen2 from "./assets/imagen2.png";
-import imagen3 from "./assets/imagen3.png";
-import imagen4 from "./assets/imagen4.png";
-import imagen5 from "./assets/imagen5.png";
-import imagen6 from "./assets/imagen6.png";
+import imagen1 from "./assets/pers4.avif";
+import imagen2 from "./assets/Chica1.avif";
+import imagen3 from "./assets/chico2.avif";
+import imagen4 from "./assets/chica2.avif";
+import imagen5 from "./assets/chico3.avif";
+import imagen6 from "./assets/chica3.avif";
 
 const cards = [
     {
         id:1,
-        title: 'Persona 1',
+        title: 'Juan - Desarrollador Web',
         image: imagen1,
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptatum."
+        description: "Experto en programación y diseño web. Transforma ideas en páginas funcionales y atractivas, superando desafíos técnicos para ofrecer resultados de alta calidad."
     },
     {
         id:2,
-        title: 'Persona 2',
+        title: 'Laura - Especialista UX/UI',
         image: imagen2,
-        description: "Lorem ipsum dolor sit amet."
+        description: "Diseñadora de experiencias y interfaces intuitivas. Crea soluciones visuales atractivas y fáciles de usar, generando interacciones fluidas y experiencias memorables."
     },
     {
         id:3,
-        title: 'Persona 3',
+        title: 'Carlos - Desarrollador Full Stack',
         image: imagen3,
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ullam natus eligendi saepe blanditiis recusandae molestiae praesentium dolore, exercitationem quasi."
+        description: "Experto en desarrollo web full stack. Crea sistemas robustos y escalables, combinando lenguajes de programación y bases de datos para soluciones eficientes."
     },
     {
         id:4,
-        title: 'Persona 4',
+        title: 'Sofía - Especialista SEO y Marketing Digital',
         image: imagen4,
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptatum."
+        description: "Apasionada del SEO y marketing digital. Optimiza la visibilidad online, investiga palabras clave y desarrolla estrategias efectivas de contenido."
     },
     {
         id:5,
-        title: 'Persona 5',
+        title: 'Alejandro - Experto en Seguridad Informática',
         image: imagen5,
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptatum."
+        description: "Experto en ciberseguridad. Protege sistemas y datos, identificando vulnerabilidades y aplicando medidas preventivas para garantizar la seguridad de los proyectos."
     },
     {
         id:6,
-        title: 'Persona 6',
+        title: 'María - Community Manager',
         image: imagen6,
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptatum."
+        description: "Profesional creativa y dinámica en redes sociales. Administra cuentas, interactúa con la comunidad y crea estrategias de contenido efectivas para aumentar la presencia en línea."
     }
 ]
 
 const CardsUS = () => {
   return (
     <>
-    <h2>Quienes somos</h2>
+    <h2>Sobre nosotros</h2>
     <div className='container d-flex justify-content-center h-100 align-items-center'>
         <div className="row">
             {
